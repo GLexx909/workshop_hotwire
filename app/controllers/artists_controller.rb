85dbf651f2ff3d2df0b6e968b0bd8cb0c8b09779
Vitaly Simonov <vitaly.simonov@roonyx.tech>
2023-07-29 15:21:58 +0300

    day-1: Task-3. Attempt-1

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  PER_PAGE = 30.freeze
+
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
@@ -8,6 +10,20 @@ class ArtistsController < ApplicationController
       render partial: "discography", locals: {artist:, albums:}
     else
       render action: :show, locals: {artist:, albums:, tracks:}
+    end
+  end
+
+  def tracks
+    artist = Artist.find(params[:id])
+    page = params[:page] ? params[:page].to_i : 1
+    offset = (page - 1) * PER_PAGE
+    tracks = artist.tracks.offset(offset).limit(PER_PAGE)
+    next_page = page + 1 if tracks.size == PER_PAGE
+
+    if turbo_frame_request?
+      render partial: "tracks/lazy_list", locals: {artist:, tracks:, page:, next_page:}
+    else
+      render action: "tracks", locals: {artist:, tracks:, page:, next_page:}
     end
   end
 
